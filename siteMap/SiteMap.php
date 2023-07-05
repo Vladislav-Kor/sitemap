6babbf3b795043a9f971a497da4153bab777263a
Vladislav <81778008+Vladislav-Kor@users.noreply.github.com>
2023-07-05 16:45:11 +0300

    changeSiteMap

--- siteMap/SiteMap.php
+++ siteMap/SiteMap.php
@@ -8,8 +8,11 @@ use app\repositories\category\CategoryRepository;
 use app\repositories\doc\DocRepository;
 use app\repositories\Hydrator;
 use app\repositories\img\ImgRepository;
+use app\repositories\product\ProductCrmRepository;
+use app\repositories\product\ProductOfferRepository;
 use app\repositories\product\ProductRepository;
 use app\services\category\CategoryService;
+use app\services\product\ProductOfferServices;
 use app\services\product\ProductServices;
 
 class SiteMap
@@ -58,11 +61,25 @@ class SiteMap
         $productServices = new ProductServices(
             new ProductRepository(new Hydrator(), new ImgRepository(new Hydrator()), new DocRepository(new Hydrator()))
         );
+        $offerServices = new ProductOfferServices(
+            new ProductOfferRepository(new Hydrator(), new ImgRepository(new Hydrator())),
+            new ProductCrmRepository(new Hydrator())
+        );
         $products = $productServices->getAllByActive();
         foreach ($products as $product) {
+            // Проверяю что категория товара не главная страница и не каталог
             if ($product->getCategory()->getValue() > 2) {
                 foreach ($categories as $parent) {
                     if ($parent->getId()->getValue() === $product->getCategory()->getValue()) {
+                        // Определяю отдельные страницы предложения товара
+                        if ($product->getPrefix()->getValue()) {
+                            $offers = $offerServices->getAllOfferByProduct($product->getId());
+                            foreach ($offers as $offer) {
+                                $urls[] .= $parent->getUrl()->getValue().'/'.$product->getUrl()->getValue().'/'.$offer->getId()->getValue();
+                            }
+
+                            break;
+                        }
                         $urls[] .= $parent->getUrl()->getValue().'/'.$product->getUrl()->getValue();
 
                         break;
@@ -80,9 +97,29 @@ class SiteMap
      */
     public function getUrl(): array
     {
+        if (!\Yii::$app->cache->get('sitemap')) {
+            $categories = $this->getCategoryUrl();
+            // сохраняю url всех активные категрии
+            $urls = $categories['urls'];
+            // нахожу все актвиные продукты в категориях и сохраняю их url
+            $urls = array_merge($urls, $this->getProductUrl($categories['categories']));
+            // кеширую данные
+            \Yii::$app->cache->set('sitemap', $urls, 3600 * 6);
+            return $urls;
+            
+        }
+        return  \Yii::$app->cache->get('sitemap');
+    }
+
+    /**
+     * changeUrl
+     * Изменяю Массив всех url Product & Category.
+     */
+    public function changeUrl(): void
+    {
         $categories = $this->getCategoryUrl();
         $urls = $categories['urls'];
-
-        return array_merge($urls, $this->getProductUrl($categories['categories']));
+        array_merge($urls, $this->getProductUrl($categories['categories']));
+        \Yii::$app->cache->set('sitemap', $urls, 3600 * 6);
     }
 }
